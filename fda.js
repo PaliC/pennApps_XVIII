a1e10f60256abceb2f7345ce9c15b9863a261d51
Labib Hussain <labibhussain@huntsman-ve701-1328.eduroam.wlan.private.upenn.edu>
2018-09-08 21:26:20 -0400

    Initial FDA commit

---
+++ fda.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var express = require('express');
+var bParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+
+app.get('/', function (req, res) {
+    request('https://api.fda.gov/drug/label.json?count=openfda.route.exact', function (error, response, body){
+        // Print the Error if one occurred:
+        console.log('error:', error);
+        // Print the response Status Code if a response was received:
+        console.log('statusCode:', response && response.statusCode);
+        // Print the HTML Body for the FDA Request
+        console.log('body:', body); 
+    });
+});
+
+app.listen(port, host, function (err) {
+    if (err)
+        throw err;
+    console.log('Your server is running on http://' + host + ':' + port + '.');
+});
